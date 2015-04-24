db2d9e366d82d6ac21257df880eecbdf91323a79
ruben <rubenfc2013@gmail.com>
2015-04-24 13:29:13 +0200

    modificada la clase comandWors y comand

--- CommandWords.java
+++ CommandWords.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 /**
  * This class is part of the "World of Zuul" application. 
  * "World of Zuul" is a very simple, text based adventure game.  
@@ -12,16 +13,27 @@
 public class CommandWords
 {
     // a constant array that holds all valid command words
-    private static final String[] validCommands = {
-            "ir", "terminar", "ayuda", "examinar", "comer", "volver", "coger", "soltar", "objetos"
-        };
-
+    //     private static final String[] validCommands = {
+    //             "ir", "terminar", "ayuda", "examinar", "comer", "volver", "coger", "soltar", "objetos"
+    //         };
+    private HashMap<String,Option> comandos;
+    private Option option;
     /**
      * Constructor - initialise the command words.
      */
     public CommandWords()
     {
         // nothing to do at the moment...
+        comandos = new HashMap<>();
+        comandos.put("go",Option.GO);
+        comandos.put("quit",Option.QUIT);
+        comandos.put("help",Option.HELP);
+        comandos.put("look",Option.LOOK);
+        comandos.put("back",Option.BACK);
+        comandos.put("take",Option.TAKE);
+        comandos.put("drop",Option.DROP);
+        comandos.put("item",Option.ITEM);
+        comandos.put("unknown",Option.UNKNOWN);
     }
 
     /**
@@ -31,12 +43,13 @@ public class CommandWords
      */
     public boolean isCommand(String aString)
     {
-        for(int i = 0; i < validCommands.length; i++) {
-            if(validCommands[i].equals(aString))
-                return true;
-        }
-        // if we get here, the string was not found in the commands
-        return false;
+        //         for(int i = 0; i < validCommands.length; i++) {
+        //             if(validCommands[i].equals(aString))
+        //                 return true;
+        //         }
+        //         // if we get here, the string was not found in the commands
+        //         return false;
+        return comandos.containsKey(aString);
     }
 
     /**
@@ -45,9 +58,33 @@ public class CommandWords
     public void showAll()
     {
         String commands = "Los comandos son: \n";
-        for(int i = 0; i < validCommands.length; i++){
-            commands = commands + validCommands[i] + " ";
+        //         for(int i = 0; i < validCommands.length; i++){
+        //             commands = commands + validCommands[i] + " ";
+        //         }
+        //         System.out.println(commands);
+        for( String comands : comandos.keySet())
+        {
+            System.out.println(commands + " ");
         }
-        System.out.println(commands);
+    }
+
+    /**
+     * Return the object Option associated with a word.
+     * @param commandWord The word to look up (as a string).
+     * @return the object Option correspondng to the paramater commandWord, or the object Option.UNKNOWN
+     *         if it is not a valid command word
+     */
+    public Option getCommandWord(String commandWord)
+    {
+        Option option = null;
+        if( comandos.get(commandWord) == null)
+        {
+            option = Option.UNKNOWN;
+        }
+        else
+        {
+            option = comandos.get(commandWord);
+        }
+        return option;
     }
 }
---
+++ Option.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Option here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum Option
+{
+   GO, 
+   QUIT, 
+   HELP, 
+   LOOK,
+   EAT,
+   BACK,
+   TAKE,
+   DROP, 
+   ITEM,
+   UNKNOWN;
+}
